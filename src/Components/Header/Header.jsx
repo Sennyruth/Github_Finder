12e8791ede87929f93daafa7d4567806e2b64f3b
Your name <your_email@example.com>
2024-06-12 15:49:03 +0300

    created the header section

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import "./Header.css"
+function Header() {
+  return (
+    <header>
+        <div className='header-section'>
+            <h1>GITHUB FINDER</h1>
+            <p>By  <a href="https://github.com/Sennyruth">Ruth Mutisya</a></p>
+            <form action="">
+                <input type="text"placeholder='enter a username' />
+                <button type='button'>Search</button>
+            </form>
+
+      
+      </div>
+    </header>
+    
+  )
+}
+
+export default Header
